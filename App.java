4962fd04e848ca4c173564e29daef64e71e8a042
kavishkarashani <rashanisamarawickrama@gmail.com>
2026-07-15 11:29:04 +0530

    Update and rename App.java to src/main/java/com/example/soc/App.java

--- App.java
+++
@@ -1,5 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Welcome to SOC-2026-W04");
-    }
-}
---
+++ src/main/java/com/example/soc/App.java
@@ -0,0 +1,20 @@
+package com.example.soc;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class App {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(App.class,args);
+
+        System.out.println("SOC Application Started");
+
+    }
+
+}
